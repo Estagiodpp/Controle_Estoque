db1bd79a025ea814e1cc8063348b48b50dfb2794
joaopedrof45 <54733346+joaopedrof45@users.noreply.github.com>
2020-03-06 17:57:53 -0300

    Add files via upload

---
+++ CONTROLE/VERIFICAR/VERIFICAALMOX/atualizar_produto.php
@@ -0,0 +1,32 @@
+<?php
+include_once("../../conexao.php");
+
+$nome = $_POST['nome'];
+$localmox = $_POST['localmox'];
+$quantidadealmox = $_POST['quantidadealmox'];
+$descalmox= $_POST['descalmox'];
+$dataalmox = $_POST['dataalmox'];
+$idalmox = $_POST['idalmox'];
+$gms = $_POST['gms'];
+$prot=$_POST['prot'];
+$locatual=$_POST['localizacao1'];
+$itematual=$_POST['nomeitem'];
+
+$sql = "UPDATE controle SET NOMEITEM = '$nome', LOCALIZACAO = '$localmox', QUANTIDADE = $quantidadealmox, DATA = '$dataalmox',DESCRICAO = '$descalmox',gms = '$gms',PROTOCOLO='$prot'" .
+       " WHERE id = $idalmox";
+$update = mysqli_query($conn,$sql);
+
+if($update){
+$ip=$_SERVER['REMOTE_ADDR'];
+$acao="Alteração";
+$tipo="Almoxarifado";
+$pat="0";
+//aqui faz o historico de transferencia na tabela history
+	$historico = "Insert into history(NOMEITEM,ORIGEM,DESTINO,TIPO,QUANTIDADE,DESCRICAO,DATA,PATRIMONIO,PROTOCOLO,ip,acao)
+values('$nome','$localmox','$localmox','$tipo','$quantidadealmox','$descalmox',now(),'$pat','$prot','$ip','$acao')
+";
+	$pesq = mysqli_query($conn, $historico);
+    header("Location: ../VERIFICAALMOX/listar_produtos.php?atualizado=$idalmox&nomeitem=$itematual&loc=$locatual&pagina=1"); 
+		
+
+}
---
+++ CONTROLE/VERIFICAR/VERIFICAPATRI/atualizar_produto.php
@@ -0,0 +1,37 @@
+<?php
+include_once("../../conexao.php");
+
+$nome = $_POST['nome'];
+$localpat = $_POST['localpat'];
+$quantidadepat = $_POST['quantidadepat'];
+$descpat= $_POST['descpat'];
+$datapat = $_POST['datapat'];
+$gms = $_POST['gms'];
+$prot=$_POST['prot'];
+$pat = $_POST['idpat'];
+$locatual=$_POST['localizacao1'];
+$patselect=$_POST['pat'];
+
+
+
+
+
+$sql = "UPDATE controle_prot SET NOMEITEM = '$nome', LOCALIZACAO = '$localpat', QUANTIDADE = '$quantidadepat', DATA = '$datapat',DESCRICAO = '$descpat',gms = '$gms',PROTOCOLO='$prot'" .
+       " WHERE PATRIMONIO = $pat";
+$update = mysqli_query($conn,$sql);
+
+if($update){
+
+
+$ip=$_SERVER['REMOTE_ADDR'];
+$acao="Alteração";
+$tipo="Patrimonio";
+$pat="0";
+//aqui faz o historico de transferencia na tabela history
+	$historico = "Insert into history(NOMEITEM,ORIGEM,DESTINO,TIPO,QUANTIDADE,DESCRICAO,DATA,PATRIMONIO,PROTOCOLO,ip,acao)
+values('$nome','$localpat','$localpat','$tipo','$quantidadepat','$descpat',now(),'$pat','$prot','$ip','$acao')
+";
+$pesq = mysqli_query($conn, $historico);
+
+    header("Location: ../VERIFICAPATRI/listar_produtos.php?atualizado=$pat&loc=$locatual&pagina=1&pat=$patselect"); 
+}
